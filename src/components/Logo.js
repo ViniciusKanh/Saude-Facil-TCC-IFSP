12b77be890cdf4d01c461b2efad160abccf4e267
Vinicius Santos <vinnyciussouza@outlook.com>
2023-11-26 16:15:43 -0300

    up
    up

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import { View, Image } from 'react-native';
 export default function Logo() {
   return (
     <View style={{ alignItems: 'center', margin: 20 }}>
-      <Image source={require('./logo.jpg')} style={{ width: 250, height: 250 }} />
+      <Image source={require('./logo.jpg')} style={{ width: 150, height: 150 }} />
     </View>
   );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,97 +1,74 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import Logo from '../components/Logo'; // Certifique-se de que o caminho está correto
 
-export default function App() {
-  // Placeholder functions for button press actions
-  const handleAlarmPress = () => alert('Alarme Pressed');
-  const handleHealthPress = () => alert('Saúde Pressed');
-  const handleRecipePress = () => alert('Receitas Pressed');
-  const handleHistoryPress = () => alert('Histórico DCNT Pressed');
-  const handleProfilePress = () => alert('Perfil Pressed');
+const HomeScreen = ({ navigation }) => {
+  const handlePress = (screen) => {
+    // Aqui você pode usar navigation.navigate(screen) para navegar
+    alert(`${screen} Pressed`);
+  };
+
+  const Icon = ({ name, label, onPress }) => (
+    <TouchableOpacity style={styles.iconWrapper} onPress={onPress}>
+      <FontAwesome name={name} size={32} color="#2e7d32" />
+      <Text style={styles.iconLabel}>{label}</Text>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>SAÚDE+FÁCIL</Text>
+      <Logo />
+      <View style={styles.iconGrid}>
+        <Icon name="bell" label="Alarme" onPress={() => handlePress('Alarme')} />
+        <Icon name="heart" label="Saúde" onPress={() => handlePress('Saúde')} />
+        <Icon name="file-text" label="Receitas" onPress={() => handlePress('Receitas')} />
+        <Icon name="line-chart" label="Histórico DCNT" onPress={() => handlePress('Histórico DCNT')} />
       </View>
-      <View style={styles.menu}>
-        <TouchableOpacity style={styles.menuItem} onPress={handleAlarmPress}>
-          <FontAwesome name="bell" size={32} color="#2e7d32" />
-          <Text style={styles.menuText}>Alarme</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem} onPress={handleHealthPress}>
-          <FontAwesome name="heart" size={32} color="#2e7d32" />
-          <Text style={styles.menuText}>Saúde</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem} onPress={handleRecipePress}>
-          <FontAwesome name="file-text" size={32} color="#2e7d32" />
-          <Text style={styles.menuText}>Receitas</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem} onPress={handleHistoryPress}>
-          <FontAwesome name="line-chart" size={32} color="#2e7d32" />
-          <Text style={styles.menuText}>Histórico DCNT</Text>
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity style={styles.profile} onPress={handleProfilePress}>
-        <FontAwesome name="user" size={32} color="#2e7d32" />
-        <Text style={styles.profileText}>Perfil</Text>
-      </TouchableOpacity>
+      <Icon name="user" label="Perfil" onPress={() => handlePress('Perfil')} style={styles.profileIcon} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  header: {
     alignItems: 'center',
-    marginTop: 50,
+    paddingTop: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  menu: {
+  iconGrid: {
     flexDirection: 'row',
     flexWrap: 'wrap',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    width: '100%',
     marginTop: 20,
   },
-  menuItem: {
+  iconWrapper: {
+    backgroundColor: '#e8f5e9',
     alignItems: 'center',
+    justifyContent: 'center',
     width: '40%',
     margin: 10,
     padding: 20,
-    backgroundColor: '#e8f5e9',
     borderRadius: 10,
-    elevation: 3, // adds shadow on Android
-    shadowOpacity: 0.3, // adds shadow on iOS
-    shadowRadius: 4,
-    shadowOffset: { width: 0, height: 2 },
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-  menuText: {
+  iconLabel: {
     marginTop: 8,
     color: '#2e7d32',
     fontWeight: 'bold',
   },
-  profile: {
-    alignItems: 'center',
-    marginBottom: 20,
-    padding: 20,
-    alignSelf: 'center',
-    backgroundColor: '#e8f5e9',
-    borderRadius: 10,
-    elevation: 3, // adds shadow on Android
-    shadowOpacity: 0.3, // adds shadow on iOS
-    shadowRadius: 4,
-    shadowOffset: { width: 0, height: 2 },
+  profileIcon: {
+    width: '80%', // Ajustar a largura conforme necessário
   },
-  profileText: {
-    marginTop: 8,
-    color: '#2e7d32',
-    fontWeight: 'bold',
-  },
 });
+
+export default HomeScreen;
